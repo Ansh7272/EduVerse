3c9f63a18c86794920b5aff8104ce0e0f3c8268f
Ansh7272 <120785449+Ansh7272@users.noreply.github.com>
2025-10-22 23:08:00 +0530

    herosection navbar

---
+++ my-app/src/app/(public)/_components/Navbar.tsx
@@ -0,0 +1,43 @@
+"use client"
+import Image from "next/image"
+import Link from "next/link"
+import Logo from '@/app/public/logo.svg'
+// import {UserDropdown} from '@/app/(public)/_components/UserDropdown'
+import { ThemeToggle } from "@/components/ui/themeToggle"
+import { authClient } from "@/lib/auth-client"
+import { buttonVariants } from "@/components/ui/button"
+import { UserDropdown } from "./UserDropdown"
+const navigation=[{
+    name:'Home',
+    href:'/'
+},{
+    name:'Courses',
+    href:'/courses'
+},{
+    name:'Dashboard',
+    href:'/dashboard'
+}]
+ export const Navbar = ()=>{
+    const {data:session,isPending} = authClient.useSession()
+    return(
+       <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur-[backdrop-filter]:bg-background/60">
+            <div className="container flex min-h-16 items-center mx-auto px-4 md:px-6 lg:px-8">
+                <Link href="/" className="flex gap-2 items-center space-x-0 mr-4">
+                <Image src={Logo} alt="Logo" className="size-11"/>
+                <span className="font-bold">AnshuLMS.</span>
+                </Link>
+
+                <nav className="hidden md:flex md:flex-1 md:items-center md:justify-between ">
+                    <div className="flex items-center space-x-2">{navigation.map((item)=>(
+                       <Link className="text-sm font-medium transition-colors hover:text-primary" key={item.name} href={item.href} >{item.name}</Link> 
+                    ))}</div>
+
+                    <div className="flex items-center space-x-4">
+                        <ThemeToggle/>
+                        {isPending?null:session?(<p><UserDropdown/> </p> ):(<><Link className={buttonVariants({variant:"secondary"})} href='/login'>Login</Link><Link className={buttonVariants()} href='/login'>Get Started</Link></>)}
+                    </div>
+                </nav>
+            </div>
+       </header>
+    )
+}
--- my-app/src/app/api/auth/[...all]/route.ts
+++ my-app/src/app/api/auth/[...all]/route.ts
@@ -1,5 +1,7 @@
 import { auth } from "@/lib/auth";
-import arcjet, {
+import ip from '@arcjet/ip'
+import arcjet from "@/lib/arcjet"
+import  {
   type ArcjetDecision,
   type BotOptions,
   type EmailOptions,
@@ -14,6 +16,8 @@ import { toNextJsHandler } from "better-auth/next-js";
 import { NextRequest } from "next/server";
 
 // The arcjet instance is created outside of the handler
+
+
 
 
 const emailOptions = {
@@ -71,17 +75,17 @@ async function protect(req: NextRequest): Promise<ArcjetDecision> {
     if (typeof body.email === "string") {
       return arcjet
         .withRule(protectSignup(signupOptions))
-        .protect(req, { email: body.email, userId });
+        .protect(req, { email: body.email, fingerprint:userId });
     } else {
       // Otherwise use rate limit and detect bot
       return arcjet
         .withRule(detectBot(botOptions))
         .withRule(slidingWindow(rateLimitOptions))
-        .protect(req, { userId });
+        .protect(req, { fingerprint:userId });
     }
   } else {
     // For all other auth requests
-    return arcjet.withRule(detectBot(botOptions)).protect(req, { userId });
+    return arcjet.withRule(detectBot(botOptions)).protect(req, { fingerprint:userId });
   }
 }
 
