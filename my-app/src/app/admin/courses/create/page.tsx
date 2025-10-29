805175a886468cbe36d8f91f5b89994a7741b183
Ansh7272 <120785449+Ansh7272@users.noreply.github.com>
2025-10-29 23:50:38 +0530

    implemented the zod validation

---
+++ my-app/src/app/admin/courses/create/page.tsx
@@ -0,0 +1,59 @@
+import { buttonVariants } from "@/components/ui/button";
+import { Card, CardAction, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { courseSchema, CourseSchemaType } from "@/lib/zodSchemas";
+import { Form } from "@/components/ui/form";
+export default function CourseCreationPage(){
+    const form = useForm<CourseSchemaType>({
+    resolver: zodResolver(courseSchema),
+    defaultValues: {
+       title:'',
+       description:'',
+       fileKey:'', 
+       price:0,
+       duration:0,
+       level:"Beginner",
+       category:"",
+       status:"Draft",
+       slug:"",
+       smallDescription:"",
+
+    },
+  })
+    // 2. Define a submit handler.
+  function onSubmit(values: CourseSchemaType) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
+    return(
+        <>
+            <div className="flex items-center gap-4">
+                <Link href="/admin/courses" className={buttonVariants({
+                    variant:"outline",
+                    size:'icon',
+                })}>
+                    <ArrowLeft className="size-4" />
+                </Link>
+                <h1 className="text-2xl font-bold">Create Courses</h1>
+            </div>
+
+            <Card>
+                <CardHeader>
+                <CardTitle>Basic Information</CardTitle>
+                <CardDescription>Provide basic information about the course</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Form {...form}>
+                    <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}></form>
+                    </Form>
+                </CardContent>
+            </Card>
+        
+        </>
+    )
+}
--- my-app/src/app/admin/courses/page.tsx
+++ my-app/src/app/admin/courses/page.tsx
@@ -1,5 +1,14 @@
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function CoursesPage(){
     return (
-        <div>Hey </div>
+        <>
+        <div className="flex items-center justify-between">
+            <h1 className="text-2xl font-bold ">Your Courses</h1>
+            <Link className={buttonVariants()} href="/admin/courses/create">Create Courses</Link>
+        </div>
+        
+        </>
     )
 }
